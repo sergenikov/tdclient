056333367efc7d89d97d3986c1f77c4aa24038de
Sergey Skovorodnikov <sg.skov@gmail.com>
2016-05-13 11:11:58 -0700

    Figuring out todoist api and OAuth mechanism.

---
+++ APILink.py
@@ -0,0 +1,18 @@
+import todoist, requests
+
+CLIENT_ID = "4a393dd72f3d4abebb2e88adc8cd2518"
+CLIENT_SECRET = "91af3f95d59d40fab7cd5aff6a57c6df"
+
+class APILink(object):
+    def __init__(self):
+        print("Creating APILink class")
+
+    def get_auth_token(self):
+        payload = {
+            "client_id" : CLIENT_ID,
+            "scope"     : "data:read",
+            "state"     : CLIENT_SECRET
+        }
+        r = requests.get("https://todoist.com/oauth/authorize", params=payload)
+        print(r.text)
+        print(r.url)
--- tdclient.py
+++ tdclient.py
@@ -1,8 +1,28 @@
-class TDClient:
+import sys, getopt
+from APILink import APILink
 
-  def __init__(self):
-    print("Started CLI Todoist client")
+usage = ("\nTODO usage")
 
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "ht")
+        if len(argv) == 0:
+            print(usage)
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
 
-def main():
-    
+    for opt, arg in opts:
+        if opt == ("-h"):
+            print(usage)
+            sys.exit()
+
+        elif opt == ("-t"):
+            print("Getting today tasks")
+
+    api_link = APILink()
+    api_link.get_auth_token()
+
+# run
+if __name__ == "__main__":
+    main(sys.argv[1:])
