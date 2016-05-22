fb71bd131d2ea38a4c2415586697a08cde72c9f7
Sergey Skovorodnikov <sg.skov@gmail.com>
2016-05-21 22:03:10 -0700

    Working on parsing the JSON from todoist and getting filtering done.

--- tdclient.py
+++ tdclient.py
@@ -3,9 +3,13 @@ import Pyro4.util
 import sys
 
 class TDClient(object):
-    def print_projects(self, tdd):
-        print("TDClient: getting list of projects")
+    def get_all(self, tdd):
+        print("TDClient: getting all items")
         tdd.print_existing_projects()
+
+    def get_today(self, tdd):
+        print("TDClient: getting today tasks")
+        tdd.get_today_tasks()
 
 
 
@@ -13,4 +17,5 @@ sys.excepthook = Pyro4.util.excepthook
 
 tdd = Pyro4.Proxy("PYRONAME:tdd.tdclient")
 tdclient = TDClient()
-tdclient.print_projects(tdd)
+# tdclient.get_all(tdd)
+tdclient.get_today(tdd)
--- tdd.py
+++ tdd.py
@@ -1,19 +1,31 @@
 import Pyro4
-import os
+import os, datetime
 
 class TDDaemon(object):
 
     _api_link = None
+    response = None
+    today = None
 
     def __init__(self, api_link):
-        print("Initializing TDDaemon")
         self._api_link = api_link
+        self.today = datetime.datetime.utcnow()
+        print("Initializing TDDaemon; time today " + self.today)
 
     def print_existing_projects(self):
         response = self._api_link.syncronize(self._api_link.get_access_token())
         for project in response['Projects']:
             print(project['name'])
 
-        user = response['user']
-        command = "echo TDD contacted " + user['full_name'] + " " + user['email'] + "| wall"
-        os.system(command)
+        # os.system("echo Printing from TDD | wall")
+
+    def get_all(self):
+        self.response = self._api_link.syncronize(self._api_link.get_access_token())
+        get_today_tasks()
+
+    def get_today_tasks(self):
+        self.response = self._api_link.syncronize(self._api_link.get_access_token())
+        items = self.response['Items']
+        for item in items:
+            if item['due_date_utc'] == self.today:
+                print(item['content'])
