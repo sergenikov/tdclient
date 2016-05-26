2bfd5e26d83dd4342c940b8d3df036cb5e948c5d
Sergey Skovorodnikov <sg.skov@gmail.com>
2016-05-26 10:35:32 -0700

    Fixed a test.

--- parser.py
+++ parser.py
@@ -13,21 +13,20 @@ class Parser(object):
 
     def get_all_tasks(self):
         items = self.response['Items']
-        # print(items)
         return items
 
     def get_today_tasks(self):
         items = self.response['Items']
         due_today = []
         date = "Sun 22 May 2016 06:59:59 +0000"
-        # d = datetime.datetime.strptime(date, "%a %d %B %Y %H:%M:%S %z")
         for item in items:
             if item['due_date'] != "None":
-                # print("adding date: " + item['due_date'])
                 d = datetime.datetime.strptime(item['due_date'], DATE_PATTERN)
-                print("d.date(): " + d.date().strftime("%d %B %Y") + "|| today: " + self.datetime_today.date().strftime("%d %B %Y"))
-
                 if d.date() == self.datetime_today.date():
                     due_today.append(item)
 
         return due_today
+
+    # Small helper to prints dates as strings
+    def print_date(self, date):
+        print("d.date(): " + date.date().strftime("%d %B %Y"))
